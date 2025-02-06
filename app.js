daaf676497b89c320da32785336029aa6a72529e
Eduardo Stoco <stoco.eduardo@gmail.com>
2025-02-06 17:24:59 -0300

    att

---
+++ app.js
@@ -0,0 +1,14 @@
+function fnCarregarDados() {
+
+const url = "https://api.github.com/users/Stoco7"
+
+fetch(url)
+  .then(resposta => {
+    return resposta.json()
+  })
+.then(dados => {
+    console.dir(dados)
+  })
+}
+
+
