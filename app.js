46bb6f43cb1be2a7adfbc8e299fc9ebddc7819c5
Eduardo Stoco <stoco.eduardo@gmail.com>
2025-02-13 15:21:53 -0300

    att

--- app.js
+++ app.js
@@ -7,8 +7,22 @@ fetch(url)
     return resposta.json()
   })
 .then(dados => {
-    console.dir(dados)
+    fnCarregarDados(dados.avatar_url, dados.name, dados.bio, dados.email, dados.html_url, dados.blog)
   })
 }
 
+function fnMostrarDados(foto, nome, resumo, email, github, instagram, blog){
+  document.getElementById("foto").src = foto
+  document.getElementById("nome").innerHTML = nome
+  document.getElementById("resumo").innerHTML = resumo + "<br><strong id = 'email'></strong>"
+  document.getElementById("email").innerHTML = email
+  document.getElementById("github").href = github
+  document.getElementById("instagram").href = instagram
+  document.getElementById("blog").href = blog
+}
+
+let botao = document.getElementById("botao-carregar-dados")
+botao.addEventListener("click", function(){
+  fnCarregarDados()
+})
 
