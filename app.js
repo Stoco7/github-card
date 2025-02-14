18f7a0f71bdf4a3bfd9ec34e87a594f8efda86cb
Eduardo Stoco <stoco.eduardo@gmail.com>
2025-02-14 15:13:59 -0300

    att

--- app.js
+++ app.js
@@ -1,28 +1,53 @@
-function fnCarregarDados() {
+function fnCarregarDados(usuario){
 
-const url = "https://api.github.com/users/Stoco7"
+  const url = "https://api.github.com/users/" + usuario
+  console.log(url)
+  // const url = "dados.json";
 
-fetch(url)
-  .then(resposta => {
-    return resposta.json()
-  })
-.then(dados => {
-    fnCarregarDados(dados.avatar_url, dados.name, dados.bio, dados.email, dados.html_url, dados.blog)
-  })
+  fetch(url)
+    .then(resposta => {
+      return resposta.json()
+    })
+    .then(dados => {
+      fnMostrarDados(dados.avatar_url, dados.name, dados.bio, dados.email, dados.html_url, dados.twitter_username)
+    })
 }
 
-function fnMostrarDados(foto, nome, resumo, email, github, instagram, blog){
+function fnMostrarDados(foto, nome, resumo, email, github, instagram, twitter) {
   document.getElementById("foto").src = foto
   document.getElementById("nome").innerHTML = nome
   document.getElementById("resumo").innerHTML = resumo + "<br><strong id = 'email'></strong>"
   document.getElementById("email").innerHTML = email
   document.getElementById("github").href = github
-  document.getElementById("instagram").href = instagram
-  document.getElementById("blog").href = blog
+  document.getElementById("instagram").href = "https://instagram.com/" + instagram
+  document.getElementById("twitter").href = "https://x.com/" + twitter
+
+  if (twitter == null) {
+    document.getElementById("twitter").style.display = "none"
+  }else{
+    document.getElementById("twitter").style.display = "inline"
+  }
+
+  if (instagram == null){
+    document.getElementById("instagram").style.display = "none"
+  }else{
+    document.getElementById("instagram").style.display = "inline"
+  }
 }
 
-let botao = document.getElementById("botao-carregar-dados")
-botao.addEventListener("click", function(){
+let botaoCarregar = document.getElementById("botao-carregar-dados")
+let botaoVoltar = document.getElementById("botao-voltar")
+botaoCarregar.addEventListener("click", function () {
+  fnCarregarDados(document.getElementById("usuario").value)
+  document.getElementById("espaco-cartao").style.display = "block"
+  document.getElementById("espaco-botao").style.display = "none"
+
+
+})
+
+botaoVoltar.addEventListener("click", function () {
   fnCarregarDados()
+  document.getElementById("espaco-cartao").style.display = "none"
+  document.getElementById("espaco-botao").style.display = "block"
 })
 
